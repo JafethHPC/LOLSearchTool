add20b2db88e8a81f8de646eb80b5deea5c643fa
JafethHPC <hjafethpachecocampos@gmail.com>
2024-06-25 10:55:11 -0400

    Changed artifactID and naming convention to 'lolsearchtool

--- api_build/src/main/java/com/lol/lolsearchtool/LOLSearchToolApplication.java
+++ api_build/src/main/java/com/lol/lolsearchtool/LOLSearchToolApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages = {"com.lol.lolsearchtool"})
+@SpringBootApplication
 @EnableTransactionManagement
 public class LOLSearchToolApplication {
 
